869abaa67c5ba70739ede68da628dfb691b18de3
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-01-06 15:32:09 +0700

    get with path/

--- server-node/index.js
+++ server-node/index.js
@@ -4,16 +4,14 @@ import fetch from "node-fetch";
 const app = express();
 app.use(express.json());
 
-app.post("/predict", async (req, res) => {
-  const response = await fetch("http://localhost:8000/predict", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(req.body)
-  });
-
-  const result = await response.json();
-  res.json(result);
+app.post("/predict", (req, res) => {
+  res.send("Server để kết nối các mô hình ngôn ngữ lớn");
 });
+
+app.get("/", (req, res) => {
+  res.send("Đây là sever dùng để kiểm tra các mô hình ngôn ngữ lớn");
+});
+
 
 app.listen(3000, () => {
   console.log("Node.js server running at http://localhost:3000");
